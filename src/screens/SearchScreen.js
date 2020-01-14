b3b7e9c9a71d2b446112339b1ca8096044d505e4
Therydusone <przem.pajak@gmail.com>
2020-01-14 20:37:25 +0100

    added error handling

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,18 +1,37 @@
 import React, { useState } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import SearchBar from '../components/SearchBar';
+import yelp from '../api/yelp';
 
 const SearchScreen = () => {
-    const [term,setTerm] = useState('')
+    const [term,setTerm] = useState('');
+    const [results, setResults] = useState([]);
+    const [errorMessage, setErrorMesage] = useState('');
+    
+    const searchApi = async () => {
+        try {
+            const response = await yelp.get('/search', {
+                params: {
+                    limit: 50,
+                    term,
+                    location: 'warsaw'
+                }
+            });
+            setResults(response.data.businesses);
+     } catch (e) {
+        setErrorMesage('Coś się zepsuło!');
+     }
+    };
+
     return (
         <View>
             <SearchBar 
             term={term} 
-            onTermChange={(newTerm) => setTerm(newTerm)}
-            onTermSubmit={() => console.log("Term submitted")}
+            onTermChange={setTerm}
+            onTermSubmit={searchApi}
             />
-            
-            <Text>Search Screen</Text>
+            {errorMessage ? <Text>{errorMessage}</Text> : null}
+            <Text>Znaleziono {results.length} wyników</Text>
         </View>
     );
 };
