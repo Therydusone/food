3739551548b30d1e834e9074788ba9e9cd672dcf
Therydusone <przem.pajak@gmail.com>
2020-01-14 19:40:47 +0100

    styling search bar

--- App.js
+++ App.js
@@ -1,19 +1,16 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+import SearchScreen from './src/screens/SearchScreen.js'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up asdao start working on your app!</Text>
-    </View>
-  );
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+
+const navigator = createStackNavigator({
+  Search: SearchScreen
+}, {
+  initialRouteName: 'Search',
+  defaultNavigationOptions: {
+    title: 'Wyszukiwarka restauracji'
+  }
 });
+
+export default createAppContainer(navigator);
