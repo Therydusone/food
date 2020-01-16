7b1dc0ca6021f123a3e6c69189b615c5e52a2b1d
Therydusone <przem.pajak@gmail.com>
2020-01-16 14:09:37 +0100

    added image, review, rating

--- src/components/ResultsDetail.js
+++ src/components/ResultsDetail.js
@@ -3,11 +3,23 @@ import { View, Image, Text, StyleSheet } from 'react-native'
 
 const ResultsDetail = ({ result }) => {
     return (
-    <View>
-        <Text>{result.name}</Text>
-    </View>);
+        <View>
+            <Image style={styles.image} source={{ uri: result.image_url}} />
+            <Text style={styles.name}>{result.name}</Text>
+            <Text>{result.rating} Gwiazdek, {result.review_count} Recenzji</Text>
+        </View>
+    );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    image: {
+        width: 250,
+        height: 120,
+        borderRadius: 4
+    },
+    name: {
+        fontWeight: 'bold',
+    }
+});
 
 export default ResultsDetail;
